9fec39681e57a7ce355fda93ef6f7fcf12e18b75
gennttllee <udooto72@gmail.com>
2022-03-21 10:28:16 +0100

    chapter 4

--- components/Contacts.js
+++ components/Contacts.js
@@ -6,22 +6,22 @@ const Contacts = () => {
         <p>Contact Me</p>
         <button className={styles.divider}>
             <a target='blank' href='https://web.facebook.com/marckwilliamz'>
-                <Image src='/static/facebook.jpg' alt='whatsapp' width={45} height={50}></Image>
+                <Image src='/static/facebook.jpg' alt='whatsapp' width={40} height={45}></Image>
             </a>
         </button>
         <button className={styles.divider}>
             <a target='blank' href='https://www.instagram.com/mark_williams_mw/'>
-                <Image src='/static/instagram.jpg' alt='whatsapp' width={45} height={50}></Image>
+                <Image src='/static/instagram.jpg' alt='whatsapp' width={40} height={45}></Image>
             </a>
         </button>
         <button className={styles.divider}>
             <a target='blank' href='https://www.linkedin.com/in/malachi-udonkenta-98a77398/'>
-                <Image src='/static/linkedin.png' alt='whatsapp' width={45} height={50}></Image>
+                <Image src='/static/linkedin.png' alt='whatsapp' width={40} height={45}></Image>
             </a>
         </button>
         <button className={styles.divider}>
             <a target='blank' href='https://wa.me/08108456793'>
-                <Image  src='/static/whatsapp.jpg' alt='whatsapp' width={55} height={60}></Image>
+                <Image  src='/static/whatsapp.jpg' alt='whatsapp' width={50} height={55}></Image>
             </a>
         </button>
     </div>
