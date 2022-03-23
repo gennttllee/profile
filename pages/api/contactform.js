9a471594f336a3f5255b85a10461d737ade54ce2
gennttllee <udooto72@gmail.com>
2022-03-23 11:30:14 +0100

    final 4

--- pages/api/contactform.js
+++ pages/api/contactform.js
@@ -1,21 +1,32 @@
 
 
-const nodemailer = require('nodemailer');
-
-export default function send(req, res) {
+export default async function send(req, res) {
+    let nodemailer = require('nodemailer')
     const transporter = nodemailer.createTransport({
-        service :'gmail',
         port: 465,
         host: "smtp.gmail.com",
         auth: {
             user: 'markwilliamz1995@gmail.com',
-            pass:'7t2j/9:g',
+            pass: '7t2j/9:g',
         },
-        secure: true
+        secure: true,
     })
 
+    await new Promise((resolve, reject) => {
+        // verify connection configuration
+        transporter.verify(function (error, success) {
+            if (error) {
+                console.log(error);
+                reject(error);
+            } else {
+                console.log("Server is ready to take our messages");
+                resolve(success);
+            }
+        });
+    });
+
     const mailData = {
-        from:'markwilliamz1995@gmail.com',
+        from:"markwilliamz1995@gmail.com",
         to: 'udooto72@gmail.com',
         subject: req.body.subject,
         text: req.body.message + " | Sent from: " + req.body.email,
@@ -23,12 +34,17 @@ export default function send(req, res) {
         ${req.body.email}</p> <p>Name : ${req.body.fname} ${req.body.lname}</p> <p> Telephone : ${req.body.number}</p>`
     }
 
-    transporter.sendMail(mailData, function (err, info) {
-        if (err)
-            console.log(err)
-        else
-            console.log(info)
-    })
-
+    await new Promise((resolve, reject) => {
+        // send mail
+        transporter.sendMail(mailData, (err, info) => {
+            if (err) {
+                console.error(err);
+                reject(err);
+            } else {
+                console.log(info);
+                resolve(info);
+            }
+        });
+    });
     res.status(200)
 };
