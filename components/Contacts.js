e3583cb6a31040649712ab259f9c8ab01cd536b5
gennttllee <udooto72@gmail.com>
2022-04-01 02:15:49 +0100

    lastly 8

--- components/Contacts.js
+++ components/Contacts.js
@@ -5,22 +5,22 @@ const Contacts = () => {
     return <div>
         <button className={styles.divider}>
             <a target='blank' href='https://web.facebook.com/marckwilliamz'>
-                <Image src='/static/facebook.jpg' alt='facebook' width={30} height={35}></Image>
+            <span className="fa-brands fa-facebook-f"></span>
             </a>
         </button>
         <button className={styles.divider}>
             <a target='blank' href='https://www.instagram.com/mark_williams_mw/'>
-                <Image src='/static/instagram.jpg' alt='instagram' width={30} height={35}></Image>
+            <span className="fa-brands fa-instagram"></span>
             </a>
         </button>
         <button className={styles.divider}>
             <a target='blank' href='https://www.linkedin.com/in/malachi-udonkenta-98a77398/'>
-                <Image src='/static/linkedin.png' alt='linkedin' width={30} height={35}></Image>
+            <span className="fa-brands fa-linkedin-in"></span>
             </a>
         </button>
         <button className={styles.divider}>
             <a target='blank' href='https://wa.me/08108456793'>
-                <Image  src='/static/whatsapp.jpg' alt='whatsapp' width={30} height={35}></Image>
+            <span className="fa-brands fa-whatsapp"></span>
             </a>
         </button>
     </div>
