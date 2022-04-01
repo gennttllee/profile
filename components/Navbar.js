99caef8d6632c5337e4b8e25319faaf8688b5396
gennttllee <udooto72@gmail.com>
2022-04-01 03:22:15 +0100

    lasting 2

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import styles from './navbar.module.css'
-import Contacts from  './Contacts'
+import Contacts from './Contacts'
 
 
 const Navbar = () => {
@@ -23,6 +23,20 @@ const Navbar = () => {
         } else { y.className = styles.responsive }
     }
 
+    const collapsed =()=>{
+        let y = document.getElementById('container')
+        if (y.className == styles.container) {
+            y.className = styles.container
+        } else if (y.className==styles.responsive){
+            setTimeout(() => {
+                if (y.className === styles.slide_back) {
+                    y.className = styles.container
+                }
+            }, 500);
+            y.className = styles.slide_back
+        }
+    }
+
     return <div id='container' className={styles.container}>
         <div className={styles.img}>
             <Image src='/static/img-1.jpg' alt='myimage' width={50} height={50}></Image>
@@ -31,16 +45,16 @@ const Navbar = () => {
             <Image src='/static/logo.jpg' alt='myimage' width={225} height={250}></Image>
         </div>
         <ul id='list' className={styles.list}>
-            <li className={styles.list_items}>
+            <li onClick={collapsed} className={styles.list_items}>
                 <a href='#home'>Home</a>
             </li>
-            <li className={styles.list_items}>
+            <li onClick={collapsed} className={styles.list_items}>
                 <a href='#features'>What i do</a>
             </li>
-            <li className={styles.list_items}>
+            <li onClick={collapsed} className={styles.list_items}>
                 <a href='#resume'>Resume</a>
             </li>
-            <li className={styles.list_items}>
+            <li onClick={collapsed} className={styles.list_items}>
                 <a href='#forms'> Contact Me</a>
             </li>
         </ul>
