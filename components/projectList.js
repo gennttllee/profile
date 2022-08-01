262a9c93edcdf2322bc1a11aa70eea40d3cd8072
gennttllee <udooto72@gmail.com>
2022-08-01 02:18:39 +0100

    m appy now

--- components/projectList.js
+++ components/projectList.js
@@ -1,29 +1,36 @@
 const projectList =[
     {
+        h1 : 'Memories',
+        image : '/static/IMG_0284.jpg',
+        skills : 'MongoDb, Express, React, Redux toolkit, Node, Google Auth, Jwt token, Rest API',
+        about : 'A fullstack CRUD social media platform ',
+        link : "https://memory-git-main-gennttllee.vercel.app"
+    },
+    {
+        h1 : 'E-commerce',
+        image : '/static/e-commerce.jpeg',
+        skills : 'Next.js, MongoDB, Git, Jwt token, Node.js, react, Paystack, Rest API,',
+        about : 'A fullstack e-commerce platform with authentication, payment and admin dashboard',
+        link : "https://store-wine-zeta.vercel.app/Main"
+    },
+    {
         h1 : 'DJ Morris Makenzee',
         image : '/static/morris.jpeg',
-        skills : 'react and Css',
+        skills : 'React ',
         about : 'DJ Morris Makenzee is a popular Nigerian dj and i happen to be the developer behind his portfolio website',
         link : "https://morris.vercel.app/"
     },
     {
         h1 : 'Dev Hire',
         image : '/static/devhire.jpeg',
-        skills : 'React, API integration, state management tool, Git and Css',
+        skills : 'React, API integration, state management tool, Git ',
         about : 'dev hire is a platform that allows users to add their favorite developers to a favorite category and can change currency based on users preference',
         link : "https://online-rust.vercel.app/"
     },
     {
-        h1 : 'E-commerce',
-        image : '/static/e-commerce.jpeg',
-        skills : 'Next.js, MongoDB, Git, Jwt token, Node.js, react, Paystack, Rest API, Css',
-        about : 'a fullstack e-commerce platform i built for a client',
-        link : "https://store-wine-zeta.vercel.app/Main"
-    },
-    {
         h1 : 'shopper',
         image : '/static/shopper.jpg',
-        skills : 'react and Css',
+        skills : 'React ',
         about : 'I built this App because i got tired of many note pad apps not including calculators.  This project takes user inputs, creates a local database and saves all input and calculations to a history folder',
         link : "https://shopper-gennttllee.vercel.app/"
     },
